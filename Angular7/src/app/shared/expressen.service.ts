7bfa53444bfde3875f1d569d7ccbd6349d799e5b
eriol726 <eriol726@student.liu.se>
2019-08-08 11:47:38 +0200

    posting latest rss feeds and assign it to the list

--- Angular7/src/app/shared/expressen.service.ts
+++ Angular7/src/app/shared/expressen.service.ts
@@ -14,31 +14,73 @@ import Axios, {AxiosInstance} from 'axios';
 })
 export class ExpressenService {
   readonly rootURL = "http://localhost:44380/api";
-  list: Expressen[];
+  list: Expressen[] = [{
+    ID: 0,
+    Title: "",
+    ImageURL: "",
+    Text: "",
+    Date: "",
+    Category: "",
+    Link: "",
+    Source: ""
+  }]
 
   private rssUrl: string = "http://www.expressen.se/Pages/OutboundFeedsPage.aspx?id=3642159&viewstyle=rss";
 
-  constructor(private http: HttpClient) { 
-    // let parser = new RSSParser();
+  constructor(private http: HttpClient, private feed: Expressen) { 
+    
+    console.log("list: ", this.list);
+
     this.http.get<any>(" https://api.rss2json.com/v1/api.json?rss_url="+this.rssUrl).toPromise().then(res  =>{
       console.log("res: ", res.items);
-      this.list = res.items ;
-      console.log("this.list: ", this.list);
-      console.log("this.list[0]: ", this.list[0]);
+      //this.list = res.items ;
+
+      res.items.forEach((item, index )=> {
+        console.log("index: ", index);
+        this.feed.Category =  "";//item.Category.length > 0 ? item.Category[0] : null ;
+        this.feed.Date = item.pubDate;
+        this.feed.Text =  item.content;
+        this.feed.Link = item.link;
+        this.feed.ImageURL = item.thumbnail;
+        this.feed.ID = index+1;
+        this.feed.Title = item.title;
+        this.feed.Source = "Expressen";
+        
+        // only adds the last item???
+        //this.list.push(this.feed);
+
+        this.putExpressen(this.feed).subscribe(res => {
+          console.log("feed inserted");
+        },
+        err =>{
+          console.log("Error: ", err);
+          debugger;
+        })
+      });
+      
+      
+      this.getExpressen().then(res =>{
+        this.list = res as Expressen[];
+      });
+      
     })
 
-    this.postExpressen();
+    
 
   }
 
-  postExpressen(){
-    console.log("post feed");
-    return this.http.post(this.rootURL+"/Expressens",null);
+  postExpressen(feed : Expressen){
+    console.log("post feed: ", feed);
+    return this.http.post(this.rootURL+"/Expressens",feed);
   }
 
   getExpressen(){
     console.log("get feed");
     return this.http.get(this.rootURL+"/Expressens").toPromise();
+  }
+
+  putExpressen(feed : Expressen){
+    return this.http.put(this.rootURL+"/Expressens/"+feed.ID, feed);
   }
   
 }
