d2c6852d0187b151eb662baefa415087e6917612
eriol726 <eriol726@student.liu.se>
2019-08-08 15:26:38 +0200

    added routes

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ExpressenComponent } from './home/expressen/expressen.component';
+import { ExpressenComponent } from './expressen/expressen.component';
+import { SvdComponent } from './svd/svd.component';
+import { NtComponent } from './nt/nt.component';
 
 const routes: Routes = [
   {path:"home", component:HomeComponent},
-  {path:"expressen", component:ExpressenComponent}
+  {path:"expressen", component:ExpressenComponent},
+  {path:"svd", component:SvdComponent},
+  {path:"nt", component:NtComponent}
 
   
 ];
