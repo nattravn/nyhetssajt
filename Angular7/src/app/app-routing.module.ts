e41aff6d79a6fe751427cc968738ed44bef6c45c
eriol726 <eriol726@student.liu.se>
2019-08-08 11:41:04 +0200

    added Expressen route

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ExpressenComponent } from './home/expressen/expressen.component';
 
 const routes: Routes = [
-  {path:"home", component:HomeComponent}
+  {path:"home", component:HomeComponent},
+  {path:"expressen", component:ExpressenComponent}
+
+  
 ];
 
 @NgModule({
