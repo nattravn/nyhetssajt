6ee4d9a2ebe41972c4e756a1b99b0a60c4bc2c92
eriol726 <eriol726@student.liu.se>
2019-08-14 22:24:30 +0200

    pushing routes to route list, updating customs

--- Angular7/src/app/app.component.ts
+++ Angular7/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomService } from './shared/custom.service';
+import { CustomComponent } from './custom/custom.component';
+import { Custom } from './shared/custom.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nyhetssajt-app';
+
+  constructor(private router: Router, private customService: CustomService) { 
+    this.customService.getCustom().then(res =>{
+      let array = res as Custom[];
+      array.forEach((item, index )=>{
+        if(index == 0){
+          //this.router.config.push({ path: array[0].Source, component: CustomComponent });
+          this.customService.customRoutes.push(array[0].Source);
+          console.log("Push: ", array[0].ID);
+          this.customService.insertCustom(array[0]);
+        }
+        
+        if(index < array.length-1 && array[index].Source != array[index+1].Source  ){
+          console.log("Push: ", array[index].Source);
+          this.customService.insertCustom(array[index]);
+          //this.router.config.push({ path: array[index].Source, component: CustomComponent });
+          this.customService.customRoutes.push(array[index].Source);
+        }
+      })
+    })
+
+    console.log("this.router: ", this.router.config);
+  }
 }
