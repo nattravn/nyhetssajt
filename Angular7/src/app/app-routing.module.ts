7fd9a1b591680892c4143e0d15ae31673778ce1f
eriol726 <eriol726@student.liu.se>
2019-08-14 22:21:41 +0200

    added routes for custom

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { ExpressenComponent } from './expressen/expressen.component';
 import { SvdComponent } from './svd/svd.component';
 import { NtComponent } from './nt/nt.component';
+import { CategoryComponent } from './category/category.component';
+import { CustomComponent } from './custom/custom.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  {path:"home", component:HomeComponent},
+  {path:"", component:HomeComponent},
   {path:"expressen", component:ExpressenComponent},
   {path:"svd", component:SvdComponent},
-  {path:"nt", component:NtComponent}
+  {path:"nt", component:NtComponent},
+  {path:"category", component:CategoryComponent},
+  {path:"admin", component:AdminComponent},
+  {path:"custom", component:CustomComponent}
 
   
 ];
