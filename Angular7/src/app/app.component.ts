b40f5d2875b2c6c859f5c686f5594fe3124a1994
eriol726 <eriol726@student.liu.se>
2019-08-17 12:23:40 +0200

    custom feeds are updated when the db.source changes

--- Angular7/src/app/app.component.ts
+++ Angular7/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CustomService } from './shared/custom.service';
 import { CustomComponent } from './custom/custom.component';
 import { Custom } from './shared/custom.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +13,64 @@ import { Custom } from './shared/custom.model';
 export class AppComponent {
   title = 'nyhetssajt-app';
 
-  constructor(private router: Router, private customService: CustomService) { 
+  constructor(private router: Router, private feed: Custom, private customService: CustomService, private http :  HttpClient ) { 
+    //http://www.svt.se/nyheter/rss.xml
+    //http://www.aftonbladet.se/rss.xml
     this.customService.getCustom().then(res =>{
-      let array = res as Custom[];
-      array.forEach((item, index )=>{
-        if(index == 0){
-          //this.router.config.push({ path: array[0].Source, component: CustomComponent });
-          this.customService.customRoutes.push(array[0].Source);
-          console.log("Push: ", array[0].ID);
+      let dbRows = res as Custom[];
+      dbRows.forEach((dbRow, dbRowIndex )=>{
+        if(dbRowIndex == 0){
+          this.http.get<any>(" https://api.rss2json.com/v1/api.json?rss_url="+dbRows[0].Rss).toPromise().then(res  =>{
+            console.log("res.length first: ", res.items.length);
+            res.items.forEach((rssItem, rssItemIndex )=> {
+              this.feed.Category =  rssItem.categories.length > 0 ? rssItem.categories[0] : null ;
+              this.feed.Date = rssItem.pubDate;
+              this.feed.Text =  rssItem.content;
+              this.feed.Link = rssItem.link;
+              this.feed.ImageURL = rssItem.thumbnail;
+              this.feed.ID = dbRowIndex+rssItemIndex+1;
+              this.feed.Title = rssItem.title;
+              this.feed.Source = dbRows[dbRowIndex].Source;
+              this.feed.Rss = dbRows[dbRowIndex].Rss;
+              this.feed.Info = dbRows[dbRowIndex].Info; 
+
+              this.customService.updateCustom(this.feed);
+            });
+          });
+
+          this.customService.customRoutes.push(dbRows[0].Source);
         }
         
-        if(index < array.length-1 && array[index].Source != array[index+1].Source  ){
-          console.log("Push: ", array[index].Source);
-          this.customService.updateCustom(array[index]);
-          this.customService.customRoutes.push(array[index+1].Source);
-          //this.router.config.push({ path: array[index].Source, component: CustomComponent });
+        // here we identify when we get a new source from the table
+        if(dbRowIndex < dbRows.length-1 && dbRows[dbRowIndex].Source != dbRows[dbRowIndex+1].Source  ){
+          console.log("Row: ", dbRows[dbRowIndex]);
+          console.log("Row: ", dbRows[dbRowIndex+1]);
           
+          this.http.get<any>(" https://api.rss2json.com/v1/api.json?rss_url="+dbRows[dbRowIndex+1].Rss).toPromise().then(res  =>{
+            console.log("res.length: ", res.items.length);
+            res.items.forEach((rssItem, rssItemIndex )=> {
+              console.log("ID: ", " index: ", dbRowIndex+rssItemIndex+1)
+              this.feed.Category =  rssItem.categories.length > 0 ? rssItem.categories[0] : null ;
+              this.feed.Date = rssItem.pubDate;
+              this.feed.Text =  rssItem.content;
+              this.feed.Link = rssItem.link;
+              this.feed.ImageURL = rssItem.thumbnail;
+              this.feed.ID = dbRowIndex+rssItemIndex+1+1;
+              this.feed.Title = rssItem.title;
+              this.feed.Source = dbRows[dbRowIndex+1].Source;
+              this.feed.Rss = dbRows[dbRowIndex+1].Rss;
+              this.feed.Info =dbRows[dbRowIndex+1].Info;
+
+
+              this.customService.updateCustom(this.feed);
+            });
+          });
+          this.customService.customRoutes.push(dbRows[dbRowIndex+1].Source);
         }
       })
 
-      array.sort((a,b) => b.Date.localeCompare(a.Date));
-      this.customService.list = array;
+      dbRows.sort((a,b) => b.Date.localeCompare(a.Date));
+      this.customService.list = dbRows;
     })
 
     console.log("this.router: ", this.router.config);
